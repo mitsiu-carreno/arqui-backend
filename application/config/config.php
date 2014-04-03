5833b1b206dfb98709758691d91081084d4dd89f
mitsiu-carreno <mitsiu.carreno@gmail.com>
2014-04-03 13:49:50 -0600

    avance de vistas

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/arqui-backend';
+$config['base_url']	= 'http://192.168.56.102/animactiva';
 
 /*
 |--------------------------------------------------------------------------
