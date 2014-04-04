23c68dc4ee25250f3b600386dc14adc25a9da8ca
mitsiu-carreno <mitsiu.carreno@gmail.com>
2014-04-04 17:56:04 -0600

    resolving

---
+++ application/controllers/imagenes.php
@@ -0,0 +1,34 @@
+<?php
+
+class Imagenes extends CI_Controller {
+    
+    function index(){
+        $this->load->view("header");
+        $this->load->view("clients/banner");
+        $this->load->view("footer");
+    }
+    
+    function subir_banner(){
+        	$config['upload_path'] = './uploads/';
+		$config['allowed_types'] = 'gif|jpg|png';
+		$config['max_size']	= '100';
+		$config['max_width']  = '1024';
+		$config['max_height']  = '768';
+
+		$this->load->library('upload', $config);
+
+		if ( ! $this->upload->do_upload())
+		{
+			$error = array('error' => $this->upload->display_errors());
+
+			$this->load->view('upload_form', $error);
+		}
+		else
+		{
+			$data = array('upload_data' => $this->upload->data());
+
+			$this->load->view('upload_success', $data);
+		}
+    }
+    
+}
