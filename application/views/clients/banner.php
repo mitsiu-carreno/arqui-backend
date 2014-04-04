632f182f8529bbc0dcf0184c8a655c5f7d0e7d0a
Fernando Medrano <fernando.medrano@upa.edu.mx>
2014-04-04 00:17:10 -0600

    validar el tamaño de la imagen en javascript antes de subirla

---
+++ application/views/clients/banner.php
@@ -0,0 +1,41 @@
+<script>
+$(function(){
+
+    $("#btn_subir").click(function(){
+        $("#inp_file").click();
+    });
+    
+    $("#inp_file").change(function(){
+        $("#alert_subir").fadeOut("slow");
+        var oFReader = new FileReader();
+        oFReader.readAsDataURL(this.files[0]);
+        oFReader.onload = function (oFREvent) {
+            var img = new Image();
+            img.src = oFREvent.target.result;
+            img.onload = function () {
+                if ((this.width != 1024) && (this.height != 227)) {
+                    //that.value="";
+                    $("#alert_subir").fadeIn("slow");
+                }
+        }
+    }
+        
+    });
+});
+</script>
+<style>
+    #inp_file {
+        display: none;
+    }
+    
+    #alert_subir {
+        display: none;
+    }
+</style>
+<img id="img_banner" />
+<div class="alert alert-danger alert-dismissable" id="alert_subir">
+  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+  <strong>¡Error!</strong> Esta imagen debe de ser de 1024 x 227.
+</div>
+<input type="file" id="inp_file" />
+<button class="glyphicon glyphicon-circle-arrow-up btn btn-default" id="btn_subir">Subir Imagen</button>
