7d5ac496b0e34c740a7f476ce972d4190e4e4539
Fernando Medrano <fernando.medrano@upa.edu.mx>
2014-04-07 01:04:57 -0500

    editar proyecto, el formulario y de crea proyecto se agrego la funcionalidad de desactivar a los recien creados

--- application/views/clients/list.php
+++ application/views/clients/list.php
@@ -20,7 +20,16 @@ $(function(){
                     tr.append($("<td />").html($("<a />").html(cliente.nombre).attr("href","<?php echo site_url(array("menus","get")) ?>/"+cliente.id)));
                     tr.append($("<td />").html(cliente.email));
                     tr.append($("<td />").html($("<button />").html("Editar").addClass("btn-editar btn btn-primary").attr("clienteid",cliente.id)));
-                    tr.append($("<td />").html($("<button />").html("Desactivar").addClass("btn-eliminar btn btn-danger").attr("clienteid",cliente.id)));
+                    tr.append($("<td />").html(
+                            $("<button />")
+                                .html("Desactivar")
+                                .addClass("btn-eliminar btn btn-danger")
+                                .attr("clienteid",cliente.id)
+                                .attr("status",1).append(
+                                        $("<span />").addClass("glyphicon-ok-circle text-success")
+                                    )
+                                )
+                            );
                     $("#tbl-list > tbody").append(tr);
                     console.log("bien: " + cliente);
                 }
@@ -50,26 +59,6 @@ $(function(){
                     $.get("<?php echo site_url(array("clients","update_status")) ?>/" + element.attr("clienteid") + "/0");
                 }
             });
-    //            $.ajax({
-    //            url : this.action,
-    //            type : "POST",
-    //            dataType: "json",
-    //            data : $(this).serialize(),
-    //            success : function(cliente){
-    //                if(!(cliente.id > 0)){
-    //                    console.log("error: " + cliente);
-    //                } else {
-    //                    var tr = $("<tr />");
-    //                    tr.append($("<td />").html(cliente.id));
-    //                    tr.append($("<td />").html($("<a />").html(cliente.nombre).attr("href","<?php echo site_url(array("menus","get")) ?>/"+cliente.id)));
-    //                    tr.append($("<td />").html(cliente.email));
-    //                    tr.append($("<td />").html($("<button />").html("Editar").addClass("btn-editar btn btn-primary").attr("clienteid",cliente.id)));
-    //                    tr.append($("<td />").html($("<button />").html("Eliminer").addClass("btn-eliminar btn btn-danger").attr("clienteid",cliente.id)));
-    //                    $("#tbl-list > tbody").append(tr);
-    //                    console.log("bien: " + cliente);
-    //                }
-    //            }
-    //        });
             
         }else{
             bootbox.confirm('¿Estás seguro de <strong><span class="text-success">activar</span></strong> este proyecto?', function(result){
@@ -88,7 +77,10 @@ $(function(){
     });
     
     $('.tooltip-status').tooltip();
-//    bootbox.confirm('¿Estás seguro de <strong><span class="text-danger">desactivar</span></strong> este proyecto?');
+    $(".btn-editar").click(function(){
+        $("#tbl-list").toggle();
+        $("#div_form_editar").removeClass("hidden").addClass("show");
+    });
 });
 </script>
 <style>
@@ -98,6 +90,7 @@ $(function(){
 </style>
 <div class="container" style="margin-top: 80px;">
     <button class="btn btn-success" id="btn-nuevo-cliente" ><span class="glyphicon glyphicon-plus"></span> Nuevo Proyecto</button>
+    <!--Table-->
     <table id="tbl-list" class="table table-hover">
         <thead>
             <tr>
@@ -120,13 +113,12 @@ $(function(){
             <?php endforeach; ?>
         </tbody>
     </table>
+    <!--New Form-->
     <div id="div_form_nuevo">
         <form method="post" action="<?php echo site_url(array("clients", "insert")) ?>" id="form_nuevo_cliente">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title" id="myModalLabel">Crear cuenta</h4>
+            <div class="page-header">
+                <h2>Crear proyecto</h2>
             </div>
-            <div class="modal-body">
                 <div class="form-group">
                     <label for="inp_nuevoNombre">Nombre</label>
                     <input type="text" name="nombre" class="form-control" id="inp_nuevoNombre" placeholder="Escriba el nombre la cuenta">
@@ -142,11 +134,33 @@ $(function(){
                     <input type="password" name="password" class="form-control" id="inp_nuevoPassword" placeholder="Escriba la contraseña">
 
                 </div>
-            </div>
-            <div class="modal-footer">
                 <button type="button" id="btn_nuevo_cancelar" class="btn btn-danger glyphicon glyphicon-remove">Cancelar</button>
                 <button type="submit" class="btn btn-success glyphicon glyphicon-ok">Crear</button>
+        </form>
+    </div>
+    <!--Edit form-->
+    <div id="div_form_editar" class="hidden">
+        <form method="post" action="<?php echo site_url(array("clients", "update")) ?>" id="form_editar_cliente">
+            <div class="page-header">
+                <h4>Editar Proyecto</h4>
             </div>
+                <div class="form-group">
+                    <label for="inp_editarNombre">Nombre</label>
+                    <input type="text" name="nombre" class="form-control" id="inp_editarNombre" placeholder="Escriba el nombre la cuenta">
+                    <small>Escriba un nombre significativo y descriptivo</small>
+                </div>
+                <div class="form-group">
+                    <label for="inp_editarEmail">Email</label>
+                    <input type="email" name="email" class="form-control" id="inp_editarEmail" placeholder="Escriba el email de la cuenta">
+                    <small>Este email servirá para ingresar a la plataforma junto con la contraseña</small>
+                </div>
+                <div class="form-group">
+                    <label for="inp_editarPassword">Contraseña <small>Dejar este campo en blanco para no editar la contraseña</small></label>
+                    <input type="password" name="password" class="form-control" id="inp_editarPassword" placeholder="Dejar este campo en blanco para no editar la contraseña">
+
+                </div>
+                <button type="button" id="btn_editar_cancelar" class="btn btn-danger glyphicon glyphicon-remove">Cancelar</button>
+                <button type="submit" class="btn btn-success glyphicon glyphicon-ok">Editar</button>
         </form>
     </div>
 </div>
