78c5f39df17346211e44e80dc08c2e93c270009f
mitsiu-carreno <mitsiu.carreno@gmail.com>
2014-04-22 19:34:26 -0500

    Change Tipo on clic

--- application/controllers/submenu.php
+++ application/controllers/submenu.php
@@ -5,10 +5,17 @@ class Submenu extends CI_Controller {
     function get($idmenu){
         $this->load->model("menu_model");
         $data = $this->menu_model->getTipo($idmenu);
+        var_dump($data["client_id"]);
         //echo json_encode(array("tipo" => $data["tipo"]));
         if($data["tipo"] == 0){
             //Cargar el submenu
             switch($data["submenu"]){
+                case 1:
+                    $this->video($idmenu);
+                    break;
+                case 2:
+                    $this->galeria($data["client_id"],$idmenu);
+                    break;
                 default :
                     $this->video($idmenu);
             }
@@ -22,6 +29,12 @@ class Submenu extends CI_Controller {
         $data = $this->menu_model->getTipo($idmenu);
         $this->load->view("submenu/botonera", array("submenu" => 1));
         $this->load->view("submenu/video", $data);
+    }
+    
+    function galeria($idcliente,$idmenu){
+        $data = array("idcliente"=>$idcliente, "idmenu" => $idmenu);
+        $this->load->view("galeria", $data);
+        
     }
 //    
 //    function get_first($idcliente){
