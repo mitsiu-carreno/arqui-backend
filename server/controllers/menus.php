311d08618f2efc0bd54a5eb08cd040553ceda272
Fernando Medrano <fernando.medrano@upa.edu.mx>
2014-04-22 19:13:53 -0500

    WebServices: Segundo - Si no hay banner mandar null

--- server/controllers/menus.php
+++ server/controllers/menus.php
@@ -30,7 +30,6 @@ class Menus extends REST_Controller
         } else {
             $this->load->model("menu_model");
             $data = array();
-            $data["banner"] = "http://cognosvideoapp.com.mx/index.php/imagenes/get_banner/imagenes/get_banner/".$idcliente;
             $data["menus"] = $this->menu_model->get($idcliente);
             if(is_array($data["menus"])){
                 $this->response($data, 200);
