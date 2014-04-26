2c9a0dbc432516b935333e9e3395226743c9a48b
Fernando Medrano <fernando.medrano@upa.edu.mx>
2014-04-26 17:41:18 -0500

    ajax loader, no es compatible con tinymce

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,3 +1,25 @@
-
+        <div id="ajax-loader-app">
+            <div class="ajax-loader-inner">
+                <img src="<?php echo base_url() ?>img/715.GIF" />
+                <div>Cargando contenido...</div>
+            </div>
+        </div>
+        <script>
+            $.ajaxSetup({
+            async: true,
+            cache: false
+//            beforeSend: function( xhr ) {
+//                $( "#ajax-loader" ).show();
+//              }
+        });
+            $( document ).ajaxStart(function() {
+                $( "#ajax-loader-app" ).show();
+                console.log("mostrando el cargador");
+              });
+              $( document ).ajaxStop(function() {
+                $( "#ajax-loader-app" ).hide();
+                console.log("ocultando el cargador");
+              });
+        </script>
     </body>
 </html>
