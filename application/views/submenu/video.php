a4cc4082a8d2b0f86218c83a52b201b1771eb68e
mitsiu-carreno <mitsiu.carreno@gmail.com>
2014-05-06 00:12:24 -0500

    Indices-Edición

--- application/views/submenu/video.php
+++ application/views/submenu/video.php
@@ -22,6 +22,16 @@
         margin-top: 10px;
         margin-bottom: 10px;
     }
+    .indice-title-input{
+        display: none;
+    }
+    .indice-contenido-input{
+        display: none;
+    }
+    .btn-indice-opcion{
+        float: right;
+        margin-bottom: 5px;
+    }
 </style>
 <div class="btn-group col-md-12">
     <form id="upload-video"  method="post" action="<?php echo base_url() ?>upload.php" enctype="multipart/form-data">
@@ -46,10 +56,11 @@
                     <?php foreach ($indices as $i): ?>
                     <!--<span><?php echo $i["contenido"]?></span>-->
                         <li class="list-group-item" idIndice="<?php echo $i["id"] ?>">
-                            <a href="#" class="btn-menu-detail"><span class="menu-title"><?php echo $i["contenido"] ?></span></a>
-                            <input type="text" value="<?php echo $i["titulo"] ?>" class=" menu-title-input" />
-                            <a href="#" class="btn-menu"><span class="menu-title"><?php echo floor($i["titulo"] /60); echo ":"; echo ($i["titulo"]%60 <10) ? "0":""; echo  $i["titulo"]%60 ?></span></a>
-                            <a href="#" class="btn_menus_eliminar btn-menu-opcion"><span class="glyphicon glyphicon-trash"></span></a>
+                            <a href="#" class="btn-indice-detail"><span class="indice-title"><?php echo floor($i["titulo"] /60); echo ":"; echo ($i["titulo"]%60 <10) ? "0":""; echo  $i["titulo"]%60 ?></span></a>
+                            <input type="text" value="<?php echo floor($i["titulo"] /60); echo ":"; echo ($i["titulo"]%60 <10) ? "0":""; echo  $i["titulo"]%60?>" class=" indice-title-input" />
+                            <a href="#" class="btn-indice-detail"><span class="indice-contenido"><?php echo $i["contenido"] ?></span></a>
+                            <input type="text" value="<?php echo $i["titulo"] ?>" class=" indice-contenido-input" />
+                            <a href="#" class="btn_indice_eliminar btn-indice-opcion"><span class="glyphicon glyphicon-trash"></span></a>
                             <!--<a href="#" class="btn-menu-opcion"><span class="glyphicon glyphicon-resize-vertical"></span></a>-->
 <!--                            <a href="#" class="btn-menu-opcion dropdown-toggle pull-right" data-toggle="dropdown"><span class="menu-tipo"><?php echo $m["tipo"] == "0" ? "sub" : "html" ?></span><span class="caret"></span></a>
                             <ul class="dropdown-menu pull-right" role="menu">
@@ -121,6 +132,26 @@
            }
    
         });
+        $("#lista-indices").delegate("li","dblclick",function(){
+        var that2 = this;
+        $(that2).find(".indice-title").hide();
+        $(that2).find("input").blur(function(){
+                $(this).hide();
+                $(that2).find(".indice-title").html($(this).val()).show();
+        });
+        $(that2).find("input").show().focus().keyup(function(e){
+            if(e.keyCode == 13){
+                $(this).hide();
+                $(that2).find(".indice-title").html($(this).val()).show();
+                var this_idIndice = $(this).closest(".list-group-item").attr("idIndice");
+                var parametros = {id: this_idIndice, "titulo": $(this).val()};
+                //$.post("http://cognosvideoapp.com.mx/index.php/menus/editar/5", $.param(parametros), "json");
+            } else  if(e.keyCode == 27) {
+                $(this).hide().val($(that2).text());
+                $(that2).find(".indice-title").show();
+            }
+        });
+    });
 
 </script>
 <script src="<?php echo base_url(); ?>js/jquery.ui.widget.js" type="text/javascript"></script>
