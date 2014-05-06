f70b912b25d69dfcf284e42e43bd5784355cdce5
mitsiu-carreno <mitsiu.carreno@gmail.com>
2014-05-06 13:01:37 -0500

    Submenu-Indices

--- application/views/submenu/video.php
+++ application/views/submenu/video.php
@@ -57,9 +57,9 @@
                     <!--<span><?php echo $i["contenido"]?></span>-->
                         <li class="list-group-item" idIndice="<?php echo $i["id"] ?>">
                             <a href="#" class="btn-indice-detail"><span class="indice-title"><?php echo floor($i["titulo"] /60); echo ":"; echo ($i["titulo"]%60 <10) ? "0":""; echo  $i["titulo"]%60 ?></span></a>
-                            <input type="text" value="<?php echo floor($i["titulo"] /60); echo ":"; echo ($i["titulo"]%60 <10) ? "0":""; echo  $i["titulo"]%60?>" class=" indice-title-input" />
-                            <a href="#" class="btn-indice-detail"><span class="indice-contenido"><?php echo $i["contenido"] ?></span></a>
-                            <input type="text" value="<?php echo $i["titulo"] ?>" class=" indice-contenido-input" />
+                            <input type="text" id="time" value="<?php echo floor($i["titulo"] /60); echo ":"; echo ($i["titulo"]%60 <10) ? "0":""; echo  $i["titulo"]%60?>" class="indice-title-input" />
+                            <a href="#" class="btn-indice-detail2"><span class="indice-contenido"><?php echo $i["contenido"] ?></span></a>
+                            <input type="text" id="indice-contenido" value="<?php echo $i["contenido"] ?>" class=" indice-contenido-input" />
                             <a href="#" class="btn_indice_eliminar btn-indice-opcion"><span class="glyphicon glyphicon-trash"></span></a>
                             <!--<a href="#" class="btn-menu-opcion"><span class="glyphicon glyphicon-resize-vertical"></span></a>-->
 <!--                            <a href="#" class="btn-menu-opcion dropdown-toggle pull-right" data-toggle="dropdown"><span class="menu-tipo"><?php echo $m["tipo"] == "0" ? "sub" : "html" ?></span><span class="caret"></span></a>
@@ -132,28 +132,54 @@
            }
    
         });
-        $("#lista-indices").delegate("li","dblclick",function(){
-        var that2 = this;
-        console.log($(this).val()); 
-        $(that2).find(".indice-title").hide();
-        $(that2).find("input").blur(function(){
-                $(this).hide();
-                $(that2).find(".indice-title").html($(this).val()).show();
+         $(".btn-indice-detail2").dblclick(function(){
+        //var that2 = this;
+        $(this).addClass("hidden");
+        $("#indice-contenido").show();
         });
-        $(that2).find("input").show().focus().keyup(function(e){
+        
+        $("#indice-contenido").keyup(function(e){
             if(e.keyCode == 13){
-                $(this).hide();
-                $(that2).find(".indice-title").html($(this).val()).show();
-                var this_idIndice = $(this).closest(".list-group-item").attr("idIndice");
-                var parametros = {id: this_idIndice, "titulo": $(this).val()};
+                $("#indice-contenido").hide();
+                $(".btn-indice-detail2").html($(this).val());
+                $(".btn-indice-detail2").removeClass("hidden");
+               // var this_idIndice = $(this).closest(".list-group-item").attr("idIndice");
+                //var parametros = {id: this_idIndice, "titulo": $(this).val()};
                 //$.post("http://cognosvideoapp.com.mx/index.php/menus/editar/5", $.param(parametros), "json");
             } else  if(e.keyCode == 27) {
-                $(this).hide().val($(that2).text());
-                $(that2).find(".indice-title").show();
+                //console.log("entrooooooo");
+                $("#indice-contenido").hide(); 
+                $("#indice-contenido").val()
+                //$(this).hide().val($(that2).text());
+                $(".btn-indice-detail2").removeClass("hidden");
             }
         });
-    });
-
+        
+        $(".btn-indice-detail").dblclick(function(){
+        //var that2 = this;
+        $(this).addClass("hidden");
+        $("#time").show();
+//        $(that2).find("time").blur(function(){
+//                $(this).hide();
+//               // $(that2).find(".indice-title").html($(this).val()).show();
+        });
+        
+        $("#time").keyup(function(e){
+            if(e.keyCode == 13){
+                $("#time").hide();
+                $(".btn-indice-detail").html($(this).val());
+                $(".btn-indice-detail").removeClass("hidden");
+               // var this_idIndice = $(this).closest(".list-group-item").attr("idIndice");
+                //var parametros = {id: this_idIndice, "titulo": $(this).val()};
+                //$.post("http://cognosvideoapp.com.mx/index.php/menus/editar/5", $.param(parametros), "json");
+            } else  if(e.keyCode == 27) {
+                //console.log("entrooooooo");
+                $("#time").hide(); 
+                $("#time").val()
+                //$(this).hide().val($(that2).text());
+                $(".btn-indice-detail").removeClass("hidden");
+            }
+        });
 </script>
 <script src="<?php echo base_url(); ?>js/jquery.ui.widget.js" type="text/javascript"></script>
 <script src="<?php echo base_url(); ?>js/jquery.iframe-transport.js" type="text/javascript"></script>
